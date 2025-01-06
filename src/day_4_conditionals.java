ce030de39a69a4898afe173bb55c9143376e8847
unknown <sudeekshamamidi@gmail.com>
2025-01-06 09:51:12 +0530

    changes made

---
+++ src/day_4_conditionals.java
@@ -0,0 +1,18 @@
+public class day_4_conditionals {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if(isWeekend){
+            if (cigars>=40 ){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (cigars>=40 && cigars <=60){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+    
