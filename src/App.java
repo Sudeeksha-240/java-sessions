fd43fef550aea27e454fa89774446e6b24514f92
unknown <sudeekshamamidi@gmail.com>
2025-01-06 09:21:22 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 10;
+        int y = 7;
+        if(x > y){
+            System.out.println("x is greater");
+        } else {
+            System.out.println("y is greater");
+        }
+    } 
+}
