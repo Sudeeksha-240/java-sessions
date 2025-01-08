560a8389ecde8aaf3fceaf3b68e96e18c4b7301d
unknown <sudeekshamamidi@gmail.com>
2025-01-08 10:31:28 +0530

    changes made

---
+++ src/day_6_problems.java
@@ -0,0 +1,27 @@
+public class day_6_problems {
+    public static void main(String[] args) {
+        /*for(int i=0;i<4;i++){
+            System.out.print("*"+" ");
+        } */
+
+        //nested loops
+        /*for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+
+        /*for(int i = 0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } */
+
+        int[] arr = new int[5];
+        System.out.println(arr[0]);
+        String[] ar = new String[4];
+        System.out.println(ar[3]);
+    }
+}
