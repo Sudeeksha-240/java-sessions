db076c02e4186f4b2faf347da2e921fd6eaa5467
unknown <sudeekshamamidi@gmail.com>
2025-01-09 13:22:55 +0530

    changes made

---
+++ src/student.java
@@ -0,0 +1,21 @@
+public class student {
+    String name;
+    int id;
+    String classNo;
+    String dept;
+    int phno;
+
+    public void talking(){
+        System.out.println("Students are talking");
+    }
+
+    public void makingNoise(){
+        System.out.println("Students are making noise");
+    }
+
+    public void coding(){
+        System.out.println("Students are coding");
+    }
+
+    
+}
---
+++ src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        student std1 = new student();
+        System.out.println(std1.name);
+        std1.name="Sudeeksha";
+        std1.id=40;
+        std1.classNo = "g2";
+        std1.dept="cse";
+        System.out.println(std1.name); 
+    }
+}
