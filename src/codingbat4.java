7c13ae466c8888fe5ed4fa5b5fea4ba12896bf4b
unknown <sudeekshamamidi@gmail.com>
2025-01-11 10:37:34 +0530

    first added

---
+++ src/codingbat4.java
@@ -0,0 +1,9 @@
+public class codingbat4 {
+    public boolean specialEleven(int n) {
+        if(n%11 ==0 || n%11 ==1){
+          return true;
+        } else {
+          return false;
+        }
+      }
+}
